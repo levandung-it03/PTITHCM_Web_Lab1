8019e8795e19135a11b2260dbe409b4665b1b061
levandung-it03 <levandung.it03@gmail.com>
2024-02-07 22:24:20 +0700

    First Commit by levandung

---
+++ src/main/java/com/ptithcm_web_lab/ptithcms1l1/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.ptithcm_web_lab.ptithcms1l1;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
